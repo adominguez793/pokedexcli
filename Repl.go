a2de9d3b73693fcec044f220a3d3e3009c6b36f4
Alejandro Dominguez <alejandrodominguez@Alejandros-MacBook-Air.local>
2024-02-10 14:56:11 -0500

    Help and Exit commands

---
+++ Repl.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommand() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func Repl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		if input == "" {
+			continue
+		}
+
+		cleanInput := cleanInput(input)
+		command := cleanInput[0]
+		availableCommands := getCommand()
+		if command != availableCommands[command].name {
+			fmt.Println("That's not a real command...")
+			continue
+		}
+		availableCommands[command].callback()
+	}
+}
+
+func cleanInput(input string) []string {
+	cleanInput := strings.ToLower(input)
+	cleanInputSlice := strings.Fields(cleanInput)
+	return cleanInputSlice
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adominguez793/pokedexcli
+
+go 1.22.0
